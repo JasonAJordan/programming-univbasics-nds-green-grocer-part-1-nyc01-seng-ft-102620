64002278b5d8a4de436b1d7fe8ac157fbc40eac7
Jason Jordan <jasonajordan0@gmail.com>
2020-10-09 19:26:11 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -36,7 +36,7 @@ def consolidate_cart(cart)
     
       item_counted = consolidated[idxinner]
       
-      if item_hash[:item] == item_counted[:item] do 
+      if item_hash[:item] == item_counted[:item] 
         item_counted[:count] = item_counted[:count] + 1
         wasFound = true
       end 
@@ -44,7 +44,7 @@ def consolidate_cart(cart)
       idxinner += 1 
     end 
     
-    if wasFound == false do
+    if wasFound == false
       new_consolidated_item = item_hash
       new_consolidated_item[:count] = 1
       consolidated.push(new_consolidated_item)
